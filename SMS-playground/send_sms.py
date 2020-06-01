521d37f35937cbd4cf207f654ddcd1860ea9e680
SirriCelles <sirricelles@gmail.com>
2020-06-01 17:12:59 +0100

    secured cridentials

--- SMS-playground/send_sms.py
+++ SMS-playground/send_sms.py
@@ -1,15 +1,15 @@
 
 from twilio.rest import Client
 
-account_sid = 'AC592825489a89fcd520ca601e7b3493b4'
-auth_token = '3169bde7e7f956d18694d80ff2e6fe8c'
+account_sid = '[AccSID]'
+auth_token = '[AuthToken]'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     from_='+12062032013',
     body='Thanks for your order! On a scale of 1-10 would you recommend [this program] to a friend? '
          'Reply with the number 1 to 10 to this message',
-    to='+237670756565'
+    to='[number]'
 )
 
 print(message.sid)
